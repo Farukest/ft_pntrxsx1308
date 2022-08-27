27fe1a4971c6fe43632b02ba6da20930b359c7a3
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-08-28 02:09:46 +0300

    initial changes

--- killpfs.sh
+++ killpfs.sh
@@ -2,15 +2,17 @@ cd /usr/bin && ./sx1302_test_loragw_reg
 if [ "$?" != "0" ]; then
 	echo "KILLED SX1308 ON KILLPFS"
     fwCount=$(pgrep -c sx1308_lora_pkt_fwd+)
+	process="sx1308_lora_pkt_fwd"
 else
 	echo "KILLED SX1302 ON KILLPFS"
     fwCount=$(pgrep -c sx1302_lora_pkt_fwd+)
+	process="sx1302_lora_pkt_fwd"
 fi
 
 if [[ $fwCount -gt 0 ]]; 
 	then
 	  echo "Killing all pktwds.."
-	  pgrep lora_pkt_fwd+ | xargs kill
+	  pgrep $process | xargs kill
 	else
 	  echo "No current pktwd process"
 fi
