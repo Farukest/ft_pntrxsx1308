df42114f61081ccf265b8764960c915cd47ca034
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-08-28 01:25:04 +0300

    initial changes

--- runall.sh
+++ runall.sh
@@ -1,32 +1,59 @@
-i=0
-while [ $i -ne 4 ]
-do
-		i=$(($i+1))
-		count=$(pgrep -c lora_pkt_fwd$i)
-		echo $count
+#!/bin/bash
+
+ETH0_MAC=`cat /sys/class/net/eth0/address | sed 's/://g'`
+MAC_PREFIX=`echo ${ETH0_MAC} | cut -c 1-6`
+MAC_POST=`echo ${ETH0_MAC} | cut -c 7-`
+REGION=`/usr/bin/region_uptd`
+GATEWAY_ID="${MAC_PREFIX}fffe${MAC_POST}"
+echo ${GATEWAY_ID}
+
+cd /usr/bin && ./sx1302_test_loragw_reg
+if [ "$?" != "0" ]; then
+    # Run SX1308 lora pkt fwd
+    if [ "${REGION}" = "EU868" ]; then
+		count=$(pgrep -c sx1308_lora_pkt_fwd)
 		if [[ $count -gt 1 ]]; 
 		then
-		  echo "Killing that pktwds.."
-		  pgrep lora_pkt_fwd$i | xargs kill
+		  echo "Killing that SX1308 pktwds.."
+		  pgrep sx1308_lora_pkt_fwd | xargs kill
 		fi
-
 		
 		if [[ $count -eq 1 ]]; 
 		then 
-			echo "ALREADY RUNNING";
+			echo "ALREADY RUNNING SX1308";
 		else
-			echo "STARTING BECAUSE IT HAS STOPPED - "pgrep lora_pkt_fwd$i;			
-			echo "$i"
-			cd /home/ft/hs_ft_pf_$i/ && make -f Makefile
-			file=/home/ft/hs_ft_pf_$i/packet_forwarder/lora_pkt_fwd$i
-			[ -x $file ] && R="Read = yes" || R="Read = No"
-			echo "$R is not executable"
-			if ! [ -x $file ]; then
-				chmod 700 $file
-				echo "$R is now executable..."
-			fi
-			cd /home/ft/hs_ft_pf_$i/packet_forwarder/ && ./lora_pkt_fwd$i &
-			sleep $((20*($i)))
-		fi       
+			sed "s/AABBCCFFFEDDEEFF/${GATEWAY_ID}/g" /etc/global_conf.json.sx1250.EU868.template > /etc/global_conf.json
+			/usr/bin/reset_lgw.sh start
+			cd /usr/bin/ && ./sx1308_lora_pkt_fwd
+		fi   
 		
-done
+	
+    else
+        echo "SX1308 region error value: ${REGION}"
+    fi
+else
+    if [ "${REGION}" = "EU868" ]; then
+        # Run SX1302 lora pkt fwd
+		count=$(pgrep -c sx1302_lora_pkt_fwd)
+		
+		if [[ $count -gt 1 ]]; 
+		then
+		  echo "Killing that SX1302 pktwds.."
+		  pgrep sx1302_lora_pkt_fwd | xargs kill
+		fi
+		
+		if [[ $count -eq 1 ]]; 
+		then 
+			echo "ALREADY RUNNING SX1302";
+		else
+			sed "s/AABBCCFFFEDDEEFF/${GATEWAY_ID}/g" /etc/global_conf.json.sx1250.EU868.template > /etc/global_conf.json
+			cd /usr/bin/ && ./sx1302_lora_pkt_fwd -c /etc/global_conf.json
+		fi   
+		
+
+    else
+        echo "SX1302 region error value: ${REGION}"
+    fi
+fi
+		    
+
