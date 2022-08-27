5903a645544b765cd34c65f1d9bcba44fcdc7390
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-08-28 01:07:27 +0300

    initial changes

--- checkmiddles.sh
+++ checkmiddles.sh
@@ -25,20 +25,20 @@ ptCount=$(pgrep -c ftbridge+)
 echo $ptCount
 if (( $ptCount > 1 )); then
   
-  echo "More than one "$ptCount".. Killing all collectors..\n"
+  echo "More than one "$ptCount".. Killing all bridges..\n"
   pgrep ftbridge+ | xargs kill
   
-  echo "Starting collectors again..\n"  
+  echo "Starting bridge again..\n"  
   cd /home/ft/ && ./runmiddles_bridge.sh
   
 else
 
 	if(( $ptCount == 0 )); 
 		then
-			echo 'No collector exist.. We must start a collector..\n'
+			echo 'No bridge exist.. We must start a bridge..\n'
 			cd /home/ft/ && ./runmiddles_bridge.sh
 		else
-			echo "Nothing to do - "$ptCount" Collector already working..\n"
+			echo "Nothing to do - "$ptCount" bridge already working..\n"
 	fi
 		
 fi
